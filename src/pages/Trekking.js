7ebac3084769ce7828936a1e4701986363e52307
Panupong mapa <panupongm.dev@gmail.com>
2023-04-20 22:58:09 +0700

    NotDev

--- src/pages/Trekking.js
+++ src/pages/Trekking.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState, useRef } from "react";
 import "../styles/Trekking.css";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 /* import react icon  */
 import { FaWalking } from "react-icons/fa";
@@ -30,39 +30,28 @@ function Trekking() {
   const [posts, setPost] = useState([]);
   const [count, setCount] = useState([]);
 
-  const optionscheckboxDistance = [
-    { label: "5 - 10 กิโลเมตร", value: "Distance-1" },
-    { label: "10 - 20 กิโลเมตร", value: "Distance-2" },
-    { label: "20 - 30 กิโลเมตร", value: "Distance-3" },
-  ];
-  const optionscheckboxLevel = [
-    { label: "Easy", value: "Level-Easy" },
-    { label: "Normal", value: "Level-Normal" },
-    { label: "Hard", value: "Level-Hard" },
-  ];
-
   const fetchCardsData = (query = {}) => {
-    console.log('--->', query)
-    axios.get("https://trekking.gistda.or.th/api/posts", {
-      params: query,
-      paramsSerializer: params => {
-        return qs.stringify(params, { arrayFormat: 'repeat' })
-      }
-    }).then((data) => {
-      setPost(data.data.results);
-      setCount(data.data.count);
-    });
+    console.log("--->", query);
+    axios
+      .get("https://trekking.gistda.or.th/api/posts", {
+        params: query,
+        paramsSerializer: (params) => {
+          return qs.stringify(params, { arrayFormat: "repeat" });
+        },
+      })
+      .then((data) => {
+        setPost(data.data.results);
+        setCount(data.data.count);
+      });
   };
   useEffect(() => {
     fetchCardsData();
   }, []);
 
-
   const fetchCategoriesData = () => {
-    axios.get("https://trekking.gistda.or.th/api/categories/")
-      .then((data) => {
-        setCategories(data.data.results);
-      });
+    axios.get("https://trekking.gistda.or.th/api/categories/").then((data) => {
+      setCategories(data.data.results);
+    });
   };
   useEffect(() => {
     fetchCategoriesData();
@@ -70,22 +59,22 @@ function Trekking() {
 
   const handleCheckboxChange = (event) => {
     const data = event.target.value;
-    let params = {}
+    let params = {};
     if (event.target.checked) {
-      params = { ...filter, categories: [...filter.categories, data] }
+      params = { ...filter, categories: [...filter.categories, data] };
       setFilter({ ...filter, categories: [...filter.categories, data] });
       console.log("✅ Checkbox is checked");
     } else {
-
-      let categories = filter.categories || []
-      let index = categories.indexOf(data)
+      let categories = filter.categories || [];
+      let index = categories.indexOf(data);
       if (index >= 0) {
-        categories.splice(index, 1)
+        categories.splice(index, 1);
         filter.categories = categories;
+      } else {
+        params = { ...filter, categories };
+        setFilter({ ...filter, categories });
+        console.log("⛔️ Checkbox is NOT checked");
       }
-      params = { ...filter, categories }
-      setFilter({ ...filter, categories });
-      console.log("⛔️ Checkbox is NOT checked");
     }
     fetchCardsData(params);
   };
@@ -111,9 +100,7 @@ function Trekking() {
   const navRef = useRef();
 
   const showNavbar = () => {
-    navRef.current.classList.toggle(
-      "filter_container"
-    );
+    navRef.current.classList.toggle("filter_container");
   };
 
   return (
@@ -143,217 +130,139 @@ function Trekking() {
         </div>
 
         <div className="container_trekking">
-
-          <div className="filter_container stylefilter" ref={navRef}  /* style={{ display: toggle ?  'none' : 'block' }} */>
-          <button onClick={showNavbar} id="btnfilter" className="btnfilterclose" ><CgClose className="iconfiter" /></button>
+          <div
+            className="filter_container stylefilter"
+            ref={navRef} /* style={{ display: toggle ?  'none' : 'block' }} */
+          >
+            <button
+              onClick={showNavbar}
+              id="btnfilter"
+              className="btnfilterclose"
+            >
+              <CgClose className="iconfiter" />
+            </button>
             <div className="container_Location">
-              
               <h3 className="style_textfilter">Location</h3>
 
-              {categories.filter(c => c.namespace === "location").map((categories) => (
-                <div key={categories.id}>
-                  <input
-                    type="checkbox"
-                    value={categories.id}
-                    // checked={filter.categories.indexOf(categories.id) >= 0}
-                    className={"style_labelfilter"}
-                    onChange={handleCheckboxChange}
-                  />
-                  &nbsp;&nbsp;&nbsp;
-                  <label htmlFor="checkbox" className="style_labelfilter">
-                    {categories.title}
-                  </label>
-                </div>
-              ))}
+              {categories
+                .filter((c) => c.namespace === "location")
+                .map((categories) => (
+                  <div key={categories.id}>
+                    <input
+                      type="checkbox"
+                      value={categories.id}
+                      // checked={filter.categories.indexOf(categories.id) >= 0}
+                      className={"style_labelfilter"}
+                      onChange={handleCheckboxChange}
+                    />
+                    &nbsp;&nbsp;&nbsp;
+                    <label htmlFor="checkbox" className="style_labelfilter">
+                      {categories.title}
+                    </label>
+                  </div>
+                ))}
             </div>
 
             <div className="container_Distance ">
               <h3 className="style_textfilter">Distance</h3>
-              {categories.filter(w => w.namespace === "distance").map((categories) => (
-                <div key={categories.id}>
-                  <input
-                    type="checkbox"
-                    value={categories.id}
-                    // checked={filter.categories.indexOf(categories.id) >= 0}
-                    className={"style_labelfilter"}
-                    onChange={handleCheckboxChange}
-                  />
-                  &nbsp;&nbsp;&nbsp;
-                  <label htmlFor="checkbox" className="style_labelfilter">
-                    {categories.title}
-                  </label>
-                </div>
-              ))}
+              {categories
+                .filter((w) => w.namespace === "distance")
+                .map((categories) => (
+                  <div key={categories.id}>
+                    <input
+                      type="checkbox"
+                      value={categories.id}
+                      // checked={filter.categories.indexOf(categories.id) >= 0}
+                      className={"style_labelfilter"}
+                      onChange={handleCheckboxChange}
+                    />
+                    &nbsp;&nbsp;&nbsp;
+                    <label htmlFor="checkbox" className="style_labelfilter">
+                      {categories.title}
+                    </label>
+                  </div>
+                ))}
             </div>
 
             <div className="container_level">
-              {/* <h3 className="style_textfilter">Level</h3> */}
-              {categories.filter(a => a.namespace === "level").map((categories) => (
-                <div key={categories.id}>
-                  <input
-                    type="checkbox"
-                    value={categories.id}
-                    // checked={filter.categories.indexOf(categories.id) >= 0}
-                    className={"style_labelfilter"}
-                    onChange={handleCheckboxChange}
-                  />
-                  &nbsp;&nbsp;&nbsp;
-                  <label htmlFor="checkbox" className="style_labelfilter">
-                    {categories.title}
-                  </label>
-                </div>
-              ))}
+              <h3 className="style_textfilter">Level</h3>
+              {categories
+                .filter((a) => a.namespace === "level")
+                .map((categories) => (
+                  <div key={categories.id}>
+                    <input
+                      type="checkbox"
+                      value={categories.id}
+                      // checked={filter.categories.indexOf(categories.id) >= 0}
+                      className={"style_labelfilter"}
+                      onChange={handleCheckboxChange}
+                    />
+                    &nbsp;&nbsp;&nbsp;
+                    <label htmlFor="checkbox" className="style_labelfilter">
+                      {categories.title}
+                    </label>
+                  </div>
+                ))}
             </div>
             <div className="container_level">
               <h3 className="style_textfilter">Average Time</h3>
-              {categories.filter(z => z.namespace === "avg_time").map((categories) => (
-                <div key={categories.id}>
-                  <input
-                    type="checkbox"
-                    value={categories.id}
-                    // checked={filter.categories.indexOf(categories.id) >= 0}
-                    className={"style_labelfilter"}
-                    onChange={handleCheckboxChange}
-                  />
-                  &nbsp;&nbsp;&nbsp;
-                  <label htmlFor="checkbox" className="style_labelfilter">
-                    {categories.title}
-                  </label>
-                </div>
-              ))}
+              {categories
+                .filter((z) => z.namespace === "avg_time")
+                .map((categories) => (
+                  <div key={categories.id}>
+                    <input
+                      type="checkbox"
+                      value={categories.id}
+                      // checked={filter.categories.indexOf(categories.id) >= 0}
+                      className={"style_labelfilter"}
+                      onChange={handleCheckboxChange}
+                    />
+                    &nbsp;&nbsp;&nbsp;
+                    <label htmlFor="checkbox" className="style_labelfilter">
+                      {categories.title}
+                    </label>
+                  </div>
+                ))}
             </div>
           </div>
 
-
           <div className="container_item">
             <div className="titleBox">
-
-              <button onClick={showNavbar} id="btnfilter" className="btnfilter" ><GoSettings className="iconfiter" /></button>
+              <button onClick={showNavbar} id="btnfilter" className="btnfilter">
+                <GoSettings className="iconfiter" />
+              </button>
               <div className="count_item">
                 <p>{count} placs</p>
               </div>
-              {/* <div className="dropdown">
-                <p>level : </p>
-                <DropdownFilter options={options} />
-              </div> */}
             </div>
 
             {/*  พื้นที่แสดง cards */}
             <div className="container_showitem">
               <>
-
-                {/*  */}
-                <Ripples>
-                  <Link to='/ShowDetails' className="Treak_card" >
-                    <div className="textitem">
-                      <h3 /* key={cards.id} */ >เขาแหลม{/* {cards.title} */}</h3>
-                      <div className="walk">
-                        <FaWalking id='walkicon' />
-                        {/* <p>{cards.id}</p> */}
-                      </div>
-                      <div className="time">
-                        <RxClock id='Clock' />
-                        {/* <p>{cards.title}</p> */}
-                      </div>
-                    </div>
-                    <img
-                      /* key={cards.id}
-                      src={cards.cover_image}  */
-                      src={require("../img/mountain2.jpg")}
-                      className="img_item zoom"
-                    />
-                  </Link>
-                </Ripples>
-                <Ripples>
-                  <Link to='/ShowDetails' className="Treak_card" >
-                    <div className="textitem">
-                      <h3 /* key={cards.id} */ >เขาแหลม{/* {cards.title} */}</h3>
-                      <div className="walk">
-                        <FaWalking id='walkicon' />
-                        {/* <p>{cards.id}</p> */}
-                      </div>
-                      <div className="time">
-                        <RxClock id='Clock' />
-                        {/* <p>{cards.title}</p> */}
-                      </div>
-                    </div>
-                    <img
-                      /* key={cards.id}
-                      src={cards.cover_image}  */
-                      src={require("../img/mountain2.jpg")}
-                      className="img_item zoom"
-                    />
-                  </Link>
-                </Ripples>
-                <Ripples>
-                  <Link to='/ShowDetails' className="Treak_card" >
-                    <div className="textitem">
-                      <h3 /* key={cards.id} */ >เขาแหลม{/* {cards.title} */}</h3>
-                      <div className="walk">
-                        <FaWalking id='walkicon' />
-                        {/* <p>{cards.id}</p> */}
-                      </div>
-                      <div className="time">
-                        <RxClock id='Clock' />
-                        {/* <p>{cards.title}</p> */}
-                      </div>
-                    </div>
-                    <img
-                      /* key={cards.id}
-                      src={cards.cover_image}  */
-                      src={require("../img/mountain2.jpg")}
-                      className="img_item zoom"
-                    />
-                  </Link>
-                </Ripples>
-                <Ripples>
-                  <Link to='/ShowDetails' className="Treak_card" >
-                    <div className="textitem">
-                      <h3 /* key={cards.id} */ >เขาแหลม{/* {cards.title} */}</h3>
-                      <div className="walk">
-                        <FaWalking id='walkicon' />
-                        {/* <p>{cards.id}</p> */}
-                      </div>
-                      <div className="time">
-                        <RxClock id='Clock' />
-                        {/* <p>{cards.title}</p> */}
-                      </div>
-                    </div>
-                    <img
-                      /* key={cards.id}
-                      src={cards.cover_image}  */
-                      src={require("../img/mountain2.jpg")}
-                      className="img_item zoom"
-                    />
-                  </Link>
-                </Ripples>
-
                 {posts.length > 0 && (
                   <>
                     {posts.map((cards) => (
                       <>
-
                         <Ripples>
-                          <Link to='/ShowDetails' className="Treak_card" >
+                          <Link to="/ShowDetails" className="Treak_card">
                             <div className="textitem">
                               <h3 key={cards.id}>{cards.title}</h3>
                               <div className="walk">
-                                <FaWalking id='walkicon' />
-                                {/* <p>{cards.id}</p> */}
+                                <FaWalking id="walkicon" />
+                                <p>{cards.id}</p>
                               </div>
                               <div className="time">
-                                <RxClock id='Clock' />
-                                {/* <p>{cards.title}</p> */}
+                                <RxClock id="Clock" />
+                                <p>{cards.title}</p>
                               </div>
                             </div>
-                            {/*  <img
+                            <img
                               key={cards.id}
                               src={cards.cover_image}
                               className="img_item zoom"
-                            /> */}
+                            />
                           </Link>
                         </Ripples>
-
                       </>
                     ))}
                   </>
@@ -365,7 +274,6 @@ function Trekking() {
       </div>
       <Footer />
     </>
-
   );
 }
 
